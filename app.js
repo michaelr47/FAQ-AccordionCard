db9362f20d169053c076e2c3bcbb49e86a9263b9
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-11-07 21:51:56 -0800

    flex start to faq section

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const questions = Array.from(document.querySelectorAll('.questions'));
 const answersDiv = Array.from(document.querySelectorAll('.answersDiv'));
 
 questions.forEach((question, i) => {
+  let button = question.querySelector('.arrowButton');
+  console.log(button);
   question.addEventListener('click', () => {
     answersDiv[i].classList.toggle('answersDiv');
   });
 })
+
 
