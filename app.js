54681c46341022b559b960af81abd7252fada9c8
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-11-15 22:10:27 -0800

    add button transition to upside down when clicked

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const answersDiv = Array.from(document.querySelectorAll('.answersDiv'));
 
 questions.forEach((question, i) => {
   let button = question.querySelector('.arrowButton');
-  console.log(button);
+
   question.addEventListener('click', () => {
     answersDiv[i].classList.toggle('answersDiv');
+    button.classList.add('animate-upsideDown');
   });
+    button.classList.remove('animate-upsideDown');
 })
 
 
