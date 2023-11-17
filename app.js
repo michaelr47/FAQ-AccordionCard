b3c7e050f7cd864ba58819c0c9bb74a4af4dd7a6
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-11-16 22:28:23 -0800

    trying diff approach to faq functionality

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const questions = Array.from(document.querySelectorAll('.questions'));
-const answersDiv = Array.from(document.querySelectorAll('.answersDiv'));
+document.addEventListener("DOMContentLoaded", function () {
+  const questions = Array.from(document.querySelectorAll(".questions"));
+  const answersDiv = Array.from(document.querySelectorAll(".answersDiv"));
 
-questions.forEach((question, i) => {
-  let button = question.querySelector('.arrowButton');
+  // questions.forEach((question, i) => {
+  for (const question of questions) {
+    // let button = question.querySelector('.arrowButton');
 
-  question.addEventListener('click', () => {
-    answersDiv[i].classList.toggle('answersDiv');
-    button.classList.add('animate-upsideDown');
-  });
-    button.classList.remove('animate-upsideDown');
-})
+    question.addEventListener("click", (e) => {
+      // for (let i = 0; i < answersDiv.length; i++) {
+      //   answersDiv[i].style.display = "none";
+      // }
 
+      const clickedQ = e.currentTarget;
+      const associatedAnswer = clickedQ.nextElementSibling;
+      const visibleAnswer = associatedAnswer.style.display === 'block';
+      
+      if (visibleAnswer) {
+        associatedAnswer.style.display = 'none';
+      }
+      associatedAnswer.style.display = 'block';
 
+      
+    });
+  }
+});
+
+// button.classList.remove('animate-upsideDown');
+// })
+// }
+// let answersDiv1 = answersDiv[0];
+// answersDiv1.style.display = 'block';
+// let question1 = questions[0];
+// let nexToQ1 = question1.nextElementSibling;
+// console.log(nexToQ1);
