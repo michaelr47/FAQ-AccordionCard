b0f7e0721efa0a89f14fbd15eb94427136235d08
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-11-17 20:00:00 -0800

    completely refactored js code, now able to toggle question answer

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const questions = Array.from(document.querySelectorAll(".questions"));
-  const answersDiv = Array.from(document.querySelectorAll(".answersDiv"));
+const questions = Array.from(document.querySelectorAll('.questions'));
+const answersDiv = Array.from(document.querySelectorAll('.answersDiv'));
 
-  // questions.forEach((question, i) => {
-  for (const question of questions) {
-    // let button = question.querySelector('.arrowButton');
+questions.forEach((question, index) => {
+  question.addEventListener('click', (e) => {
+    const clickedQ = e.currentTarget;
+    const associatedAnswer= answersDiv[index];
+    
+    const isAnswerVisible = associatedAnswer.classList.contains('show-answer');
 
-    question.addEventListener("click", (e) => {
-      // for (let i = 0; i < answersDiv.length; i++) {
-      //   answersDiv[i].style.display = "none";
-      // }
-
-      const clickedQ = e.currentTarget;
-      const associatedAnswer = clickedQ.nextElementSibling;
-      const visibleAnswer = associatedAnswer.style.display === 'block';
-      
-      if (visibleAnswer) {
-        associatedAnswer.style.display = 'none';
-      }
-      associatedAnswer.style.display = 'block';
-
-      
-    });
-  }
+    // Toggle visibility of the associated answer
+    if (isAnswerVisible) {
+      // Hide answer
+      associatedAnswer.classList.remove('show-answer');
+    } else {
+      // Hide all other answers before displaying the clicked answer
+      answersDiv.forEach(answer => {
+        answer.classList.remove('show-answer');
+      });
+       // Display the answer
+      associatedAnswer.classList.add('show-answer');
+    }
+  });
 });
-
-// button.classList.remove('animate-upsideDown');
-// })
-// }
-// let answersDiv1 = answersDiv[0];
-// answersDiv1.style.display = 'block';
-// let question1 = questions[0];
-// let nexToQ1 = question1.nextElementSibling;
-// console.log(nexToQ1);
