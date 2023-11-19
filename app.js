ab25e74c28378c9f38ba80b26c0226275a875dc8
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-11-18 21:48:54 -0800

    add detailed comments

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+// get Elements from the DOM
 const questions = Array.from(document.querySelectorAll('.questions'));
 const answersDiv = Array.from(document.querySelectorAll('.answersDiv'));
-
+// looping through questions
 questions.forEach((question, index) => {
+  // adding click event on each question
   question.addEventListener('click', (e) => {
     const clickedQ = e.currentTarget;
     const associatedAnswer= answersDiv[index];
-    
+    // visible answer state
     const isAnswerVisible = associatedAnswer.classList.contains('show-answer');
 
     // Toggle visibility of the associated answer
