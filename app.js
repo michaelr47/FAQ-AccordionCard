0732685fd98e703e26d02cf6947c71d523c74588
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-11-18 22:06:53 -0800

    add small animation on arrows when question is clicked

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // get Elements from the DOM
 const questions = Array.from(document.querySelectorAll('.questions'));
 const answersDiv = Array.from(document.querySelectorAll('.answersDiv'));
+
 // looping through questions
 questions.forEach((question, index) => {
   // adding click event on each question
   question.addEventListener('click', (e) => {
     const clickedQ = e.currentTarget;
+    // button in question thats clicked
+    const associatedButton = question.querySelector('.arrowButton');
+    associatedButton.classList.toggle('animate-upsideDown');
+
     const associatedAnswer= answersDiv[index];
     // visible answer state
     const isAnswerVisible = associatedAnswer.classList.contains('show-answer');
@@ -14,13 +19,15 @@ questions.forEach((question, index) => {
     if (isAnswerVisible) {
       // Hide answer
       associatedAnswer.classList.remove('show-answer');
+      
     } else {
       // Hide all other answers before displaying the clicked answer
-      answersDiv.forEach(answer => {
+      answersDiv.forEach((answer, i) => {
         answer.classList.remove('show-answer');
       });
        // Display the answer
       associatedAnswer.classList.add('show-answer');
+      
     }
   });
 });
